ae50a7a4898e901d886c87b1859e08904980ba70
wlwolf1227 <wlwolf1227@yahoo.com>
2022-10-17 11:01:04 -0400

    added profiles model and linked to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_143106) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_145826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_143106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stardate"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "username"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_143106) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
