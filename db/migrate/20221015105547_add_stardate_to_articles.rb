067159a974cdcfcc721baea4fde2a742c5ad44f5
wlwolf1227 <wlwolf1227@yahoo.com>
2022-10-17 10:18:36 -0400

    scaffolded articles

---
+++ db/migrate/20221015105547_add_stardate_to_articles.rb
@@ -0,0 +1,5 @@
+class AddStardateToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_column :articles, :stardate, :string
+  end
+end
---
+++ spec/views/articles/show.html.tailwindcss_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "articles/show", type: :view do
+  before(:each) do
+    assign(:article, Article.create!(
+      title: "Title",
+      stardate: "Stardate",
+      body: "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/Stardate/)
+    expect(rendered).to match(/MyText/)
+  end
+end
