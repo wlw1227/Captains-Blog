7dd1a665f34410c5129b9369aed3cbd75d2037ee
wlwolf1227 <wlwolf1227@yahoo.com>
2022-10-17 11:13:28 -0400

    associated user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_145826) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_150255) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_145826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stardate"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -44,5 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_145826) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "profiles", "users"
 end
