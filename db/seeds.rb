ce2241644971b35f176607e5ea89a53422d78790
wlwolf1227 <wlwolf1227@yahoo.com>
2022-10-25 06:26:52 -0400

    added categories and join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create email: 'captain@example.com', password: 'password'
+
+Category.create [
+  {name: 'Official Log'},
+  {name: 'Supplemental Log'}
+]
