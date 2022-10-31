bd58520864a6ae9239e3796cb3f0620e51e4604b
wlwolf1227 <wlwolf1227@yahoo.com>
2022-10-31 15:31:48 -0400

    added validations to all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, uniqueness: true
+  validates :email, length: { in: 5..50 }
+  validates :email, format: { with: /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i }
+  validates :password, confirmation: true
+
   has_one :profile
   has_many :articles, -> { order 'published_at DESC, title ASC' }, dependent: :destroy
   has_many :replies, through: :articles, source: :comments
