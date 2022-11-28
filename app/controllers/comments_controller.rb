bb1edcdbba4973d01c721a4c8e9671cf1c0b63f2
wlwolf1227 <wlwolf1227@yahoo.com>
2022-11-28 11:01:38 -0500

    added resources in routes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+  before_action :load_article
+
+  def create
+    @comment = @article.comments.new(comment_params)
+    if @comment.save 
+      redirect_to @article, notice: 'Thanks for your comment'
+    else
+      redirect_to @article, notice: 'Unable to add comment'
+    end
+  end
+
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @article, notice: 'Comment deleted'
+  end
+
+  private
+
+  def load_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :email, :body)
+  end
+  
+end
