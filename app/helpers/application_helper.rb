e9cbc6636cfd23ce380337e928d18c9cd16d8f09
wlwolf1227 <wlwolf1227@yahoo.com>
2023-01-09 11:25:16 -0500

    cleaned up helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def submit_or_cancel(form, name="Cancel")
+  def submit_or_cancel(form, name= 'Cancel')
     form.submit + " or " + link_to(name, 'javascript:history.go(-1);', class: 'cancel')
   end
 end
