686f1902b595d47364b3edb90ab8783908864295
wlwolf1227 <wlwolf1227@yahoo.com>
2023-01-18 22:33:37 -0500

    fixed viewing new articles page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   def authenticate
     logged_in? || access_denied
   end
