a63dd29fafb12fe29de2950920253a0065d1c1ca
wlwolf1227 <wlwolf1227@yahoo.com>
2023-02-27 09:35:09 -0500

    changed header to only show sign out when user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-
-  resources :users, only: [:show] do
-    resources :profiles, only: %i[new create edit update]
-  end
 end
